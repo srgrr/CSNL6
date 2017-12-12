d82fd825460b0cdbdf144633b034e0d5504420a2
sergiorg <sergi9rr9r@gmail.com>
2017-12-12 19:36:53 +0100

    add section 4 plot

--- plot_bam_kprime.py
+++ plot_bam_kprime.py
@@ -2,6 +2,9 @@ import subprocess
 import matplotlib.pyplot as plt
 import sys
 
+def f(n0, m0, t):
+  return float(2.0 * m0 * t) / float(n0)
+
 def main():
   n0, m0, T = list(map(int, sys.argv[1:4]))
   timesteps = list(map(int, sys.argv[4:]))
@@ -12,7 +15,7 @@ def main():
     xs[ts] = []
     ys[ts] = []
   lines = subprocess.check_output(
-    ['./bam_kprime', str(n0), str(m0), str(T), *sys.argv[4:]]
+    ['./bam_kprime_no_growth', str(n0), str(m0), str(T), *sys.argv[4:]]
   ).decode('utf-8').split('\n')
   for line in lines:
     if line:
@@ -22,6 +25,7 @@ def main():
   plt.figure('Evolution of kprime')
   for (k, x) in xs.items():
     plt.plot(x, ys[k])
+  plt.plot(x, [f(n0, m0, k) for k in x])
   plt.show()
 
 
